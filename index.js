f8af0d68917a035495108aaaa0a3696a6225f185
Haha <107977298+dai-luk-yea@users.noreply.github.com>
2022-06-24 14:01:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -26,6 +27,12 @@ app.get('/api/courses/:id', (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.post('/api/courses', (req, res) => {
+   const { error } = validateCourse(req.body);
+   if (error) {
+      res.status(400).send(error.details[0].message);
+      return;
+   }
+
    const course = {
       id: courses.length + 1,
       name: req.body.name
@@ -33,6 +40,36 @@ app.post('/api/courses', (req, res) => {
    courses.push(course);
    res.send(course);
 });
+
+app.post('/api/courses', (req, res) => {
+   if (error) {
+      res.status(400).send(error.details[0].message);
+      return;
+   }
+});
+
+app.put('/api/courses/:id', (req, res) =>{
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send('The course with the given ID was not found.');
+
+   
+   const { error } = validateCourse(req.body);
+   if (error) {
+      res.status(400).send(error.details[0].message);
+      return;
+   }
+
+   course.name = req.body.name;
+   res.send(course);
+});
+
+function validateCourse(course) {
+   const schema = {
+      name: Joi.string().min(3).required()
+   };
+
+   return Joi.validate(course, schema);
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
